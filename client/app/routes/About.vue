2393fbc0500a3e332cfe8a17aeb7fe71695bcd87
Lukas Sorensen <lsorensen@sportsdigita.com>
2022-05-15 20:13:04 -0500

    setup other routes

---
+++ client/app/routes/About.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div class="route about">
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'About'
+}
+</script>
+<style lang="scss">
+    .about.route {
+
+    }
+</style>
---
+++ client/app/routes/Contact.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div class="route contact">
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'Contact'
+}
+</script>
+<style lang="scss">
+    .contact.route {
+
+    }
+</style>
