106b9277dd04d3e1b2e9882903fa4b573f0ac3e9
Lukas Sorensen <lsorensen@sportsdigita.com>
2022-08-06 19:08:28 -0500

    fix footer

--- client/app/components/Footer.vue
+++ client/app/components/Footer.vue
@@ -1,10 +1,12 @@
 <template lang="html">
     <footer>
-        <div class="cert cpdt">
+        <div class="col-xs-12 cert-container">
+            <div class="cert cpdt">
+            </div>
+            <div class="cert fear-free">
+            </div>
+            <div class="cert sapdt"></div>
         </div>
-        <div class="cert fear-free">
-        </div>
-        <div class="cert sapdt"></div>
     </footer>
 </template>
 <script>
@@ -13,47 +15,49 @@ export default {
 }
 </script>
 <style lang="scss">
-    @import "@/scss/_variables";
-    footer {
-        width: 100%;
-        height: 200px;
-        background: white;
-        padding: 65px;
-        box-sizing: border-box;
-        z-index: 2147483647;
-        position: absolute;
-        scroll-snap-align: end;
-    }
+@import "@/scss/_variables";
+
+.cert-container {
+    width: 100%;
+    height: 200px;
+    background: white;
+    padding: 65px;
+    box-sizing: border-box;
+    z-index: 2147483647;
+    position: absolute;
+    scroll-snap-align: end;
+    display: flex;
+    justify-content: space-between;
 
     .cert {
         background-size: contain;
         background-position: center;
         background-repeat: no-repeat;
         height: 100%;
-        width: 30%;
+        width: 29%;
         display: inline-block;
-        margin-right: 15px;
-        margin-left: 15px;
     }
+}
 
-    .cpdt {
-        background-image: url("assets/CPDT.png");
-    }
 
-    .fear-free {
-        background-image: url("assets/FEAR_FREE_CERT.png");
-    }
+.cpdt {
+    background-image: url("assets/CPDT.png");
+}
 
-    .sapdt {
-        background-image: url("assets/sapdt.png");
-    }
+.fear-free {
+    background-image: url("assets/FEAR_FREE_CERT.png");
+}
 
-    @media (max-width: $screen-md) {
-        footer {
-            display: flex;
-            justify-content: space-between;
-            height: 120px;
-            padding: 20px 40px;
-        }
+.sapdt {
+    background-image: url("assets/sapdt.png");
+}
+
+@media (max-width: $screen-md) {
+    footer {
+        display: flex;
+        justify-content: space-between;
+        height: 120px;
+        padding: 20px 40px;
     }
+}
 </style>
