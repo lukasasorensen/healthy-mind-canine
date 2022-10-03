41889ad9c0e84f50d50cee3312b361e4802a0793
Lukas Sorensen <lsorensen@sportsdigita.com>
2022-10-03 17:02:35 -0500

    fix express server to work with non hash routes

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 const path = require('path')
 
-app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', function (req,res) {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
