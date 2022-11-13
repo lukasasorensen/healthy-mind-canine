68f1147b26568cb2e5d7d67a652f72140bc9ef67
Lukas Sorensen <lsorensen@sportsdigita.com>
2022-11-13 16:50:12 -0600

    mobile styling for pricing

--- client/app/routes/Pricing.vue
+++ client/app/routes/Pricing.vue
@@ -2,71 +2,81 @@
     <div class="route pricing">
         <section class="section section-1 pricing">
             <div class="pricing-info-container">
+
                 <div class="pricing-header-container">
                     <h1 class="pricing-header secondary-text">Pricing</h1>
                 </div>
                 <div class="package-disclaimer w-100 text-center">
-                    <p><i>*Initial Consult is required before purchasing packages.</i></p>
+                    <p><i>*Initial Consult is required before purchasing programs.</i></p>
                 </div>
-
-                <div class="col-xs-12 col-md-4 pricing-panel-container">
-                    <div class="pricing-panel">
-                        <div class="col-xs-12 pricing-panel-header">
-                            <h2>Initial Consultation and Assessment</h2>
-                            <p class="pricing-panel-price">$175</p>
+                <div class="pricing-panels-container">
+                    <div class="col-xs-12 col-sm-4 pricing-panel-container">
+                        <div class="pricing-panel">
+                            <div class="col-xs-12 pricing-panel-header">
+                                <h2>Initial Consultation and Assessment</h2>
+                                <p class="pricing-panel-price">$175</p>
+                            </div>
+                            <div class="pricing-panel-details">
+                                <ul>
+                                    <li>90 minute private session</li>
+                                    <li>Review of your dogs history and goals</li>
+                                    <li>Assessment of your dog</li>
+                                    <li>Overview of what training will look like</li>
+                                    <li>Discuss tech set up</li>
+                                    <li>A recording of your session</li>
+                                    <li>Resources & handouts</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="pricing-panel-details">
-                            <ul>
-                                <li>90 minute private session</li>
-                                <li>Review of your dogs history and goals</li>
-                                <li>Assessment of your dog</li>
-                                <li>Overview of what training will look like</li>
-                                <li>Discuss tech set up</li>
-                                <li>A recording of your session</li>
-                                <li>Resources & handouts</li>
-                            </ul>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-4 pricing-panel-container">
+                        <div class="pricing-panel">
+                            <div class="col-xs-12 pricing-panel-header">
+                                <h2>4 Week Program</h2>
+                                <p class="pricing-panel-price">$800</p>
+                            </div>
+                            <div class="pricing-panel-details">
+                                <ul>
+                                    <li>4 weekly zoom check-ins</li>
+                                    <li>5 individualized training plans for the pet parent to conduct each week</li>
+                                    <li>Access to the SA Pro Trainer App</li>
+                                    <li>Daily messaging support M - F</li>
+                                    <li>Unlimited video review from your trainer</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-4 pricing-panel-container">
+                        <div class="pricing-panel">
+                            <div class="col-xs-12 pricing-panel-header">
+                                <h2>8 Week Program</h2>
+                                <p class="pricing-panel-price">$1,350 (save $250!)</p>
+                            </div>
+                            <div class="pricing-panel-details">
+                                <ul>
+                                    <li>8 weekly zoom check-ins</li>
+                                    <li>5 individualized training plans for the pet parent to conduct each week</li>
+                                    <li>Access to the SA Pro Trainer App</li>
+                                    <li>Daily messaging support M - F</li>
+                                    <li>Unlimited video review from your trainer</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-xs-12 col-md-4 pricing-panel-container">
-                    <div class="pricing-panel">
-                        <div class="col-xs-12 pricing-panel-header">
-                            <h2>4 Week Package</h2>
-                            <p class="pricing-panel-price">$800</p>
+                <div class="more-services">
+                    <div class="pricing-table">
+                        <div class="dotted-table-row">
+                            <p class="dotted-table-left">1 Hour as-needed Session</p>
+                            <div class="dotted-table-line"></div>
+                            <p class="dotted-table-right">$175</p>
                         </div>
-                        <div class="pricing-panel-details">
-                            <ul>
-                                <li>4 weekly zoom check-ins (1 per week)</li>
-                                <li>5 individualized training plans for the pet parent to conduct each week (20 total)
-                                </li>
-                                <li>Access to the SA Pro Trainer App</li>
-                                <li>Daily messaging support M - F</li>
-                                <li>Unlimited video review from your trainer</li>
-                            </ul>
-                        </div>
+                        <small class="pricing-submessage">*to supplement DIY programs</small>
                     </div>
                 </div>
-
-                <div class="col-xs-12 col-md-4 pricing-panel-container">
-                    <div class="pricing-panel">
-                        <div class="col-xs-12 pricing-panel-header">
-                            <h2>8 Week Package</h2>
-                            <p class="pricing-panel-price">$1,350 (save $250!)</p>
-                        </div>
-                        <div class="pricing-panel-details">
-                            <ul>
-                                <li>8 weekly zoom check-ins (1 per week)</li>
-                                <li>5 individualized training plans for the pet parent to conduct each week (40 total).
-                                </li>
-                                <li>Access to the SA Pro Trainer App</li>
-                                <li>Daily messaging support M - F</li>
-                                <li>Unlimited video review from your trainer</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
                 <div class="scroll-padding"></div>
             </div>
         </section>
@@ -108,17 +118,23 @@ export default {
         color: $secondary-color;
     }
 
+    .pricing-panels-container {
+        display: inline-block;
+    }
+
     .pricing-panel-container {
         padding: 10px;
 
         .pricing-panel {
-            padding: 20px 35px;
+            padding: 30px 25px 20px 30px;
             background: #e1cdec;
             border-radius: 10px;
+            height: 500px;
+            max-width: 390px;
 
             &:hover {
                 background: #98609e;
-                box-shadow: 0 0 11px rgba(33,33,33,.2); 
+                box-shadow: 0 0 11px rgba(33, 33, 33, .2);
 
                 .pricing-panel-details {
                     ul li {
@@ -127,7 +143,9 @@ export default {
                 }
 
                 .pricing-panel-header {
-                    h2, .pricing-panel-price {
+
+                    h2,
+                    .pricing-panel-price {
                         color: #fff;
                     }
                 }
@@ -153,9 +171,53 @@ export default {
 
                     li {
                         margin-top: 10px;
+                        font-size: 15px;
+                        list-style-image: url("assets/pawprint.svg");
+
+                        &::marker {
+                            width: 20px;
+                            height: 20px;
+                            color: $secondary-color;
+                        }
                     }
                 }
             }
+        }
+    }
+
+    .more-services {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        margin-top: 40px;
+
+        .pricing-table {
+            width: 100%;
+            max-width: 700px;
+            margin: 20px auto;
+        }
+
+        .dotted-table-row {
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+
+            .dotted-table-line {
+                border-bottom: 2px dotted $secondary-color;
+                flex: 1;
+                margin-bottom: 8px;
+                margin: 8px 4px;
+            }
+
+            .dotted-table-right {
+                color: $secondary-color;
+                font-weight: bolder;
+            }
+        }
+
+        .pricing-submessage {
+            margin-left: 20px;
+            color: $primary-color;
         }
     }
 
@@ -185,6 +247,17 @@ export default {
             }
 
 
+        }
+    }
+
+    @media(max-width: $screen-md) {
+        .pricing-info-container {
+
+            .pricing-panel-container {
+                display: flex;
+                justify-content: center;
+                margin-bottom: 30px;
+            }
         }
     }
 }
